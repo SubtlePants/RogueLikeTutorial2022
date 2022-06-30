0c90c515ff2c9a1df03922b221fa83174a3fbd65
jerrett.hoffman <jerrett.hoffman@workday.com>
2022-06-30 10:06:31 -0700

    Moves the @ symbol around

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python3
 import tcod
 
+from actions import MovementAction, EscapeAction
+from input_handlers import EventHandler
+
+
 def main() -> None:
     screen_width = 80
     screen_height = 50
@@ -10,6 +14,8 @@ def main() -> None:
 
     tileset = tcod.tileset.load_tilesheet("dejavu10x10_gs_tc.png", 32, 8, tcod.tileset.CHARMAP_TCOD)
 
+    event_handler = EventHandler()
+
     with tcod.context.new_terminal(screen_width, screen_height, tileset=tileset, title="Yet Another Roguelike Tutorial", vsync=True) as context:
         root_console = tcod.Console(screen_width, screen_height, order="F")
         while True:
@@ -18,7 +24,16 @@ def main() -> None:
             context.present(root_console)
 
             for event in tcod.event.wait():
-                if event.type == "QUIT":
+                action = event_handler.dispatch(event)
+
+                if action is None:
+                    continue
+
+                if isinstance(action, MovementAction):
+                    player_x += action.dx
+                    player_y += action.dy
+
+                elif isinstance(action, EscapeAction):
                     raise SystemExit()
 
 if __name__ == "__main__":
