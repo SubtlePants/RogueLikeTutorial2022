ef9b232221c8a34192529f1029fa2a31a0dce506
jerrett.hoffman <jerrett.hoffman@workday.com>
2022-11-19 23:31:25 -0800

    Implements FOV, end of tutorial Part 4

--- procgen.py
+++ procgen.py
@@ -5,6 +5,7 @@ from typing import Iterator, List, Tuple, TYPE_CHECKING
 
 import tcod
 
+import entity_factories
 from game_map import GameMap
 import tile_types
 
@@ -41,7 +42,21 @@ class RectangularRoom:
         )
 
 
-def tunnel_between(start: Tuple[int, int], end: Tuple[int, int]) -> Iterator[Tuple[int, int]]:
+def place_entities(room: RectangularRoom, dungeon: GameMap, maximum_monsters: int) -> None:
+    number_of_mosters = random.randint(0, maximum_monsters)
+
+    for i in range(number_of_mosters):
+        x = random.randint(room.x1 + 1, room.x2 - 1)
+        y = random.randint(room.y1 + 1, room.y2 - 1)
+
+        if not any (entity.x == x and entity.y == y for entity in dungeon.entities):
+            if random.random() < 0.8:
+                entity_factories.orc.spawn(dungeon, x, y)
+            else:
+                entity_factories.troll.spawn(dungeon, x, y)
+
+
+def tunnel_between(start: Tuple[int, int], end: Tuple[int, int]) -> [Tuple[int, int]]:
     """Return an L-shaped tunnel between these two points."""
     x1, y1 = start
     x2, y2 = end
@@ -65,10 +80,11 @@ def generate_dungeon(
         room_max_size: int,
         map_width: int,
         map_height: int,
+        max_monsters_per_room: int,
         player: Entity
 ) -> GameMap:
     """Generate a new dungeon map."""
-    dungeon = GameMap(map_width, map_height)
+    dungeon = GameMap(map_width, map_height, entities={player})
 
     rooms: List[RectangularRoom] = []
 
@@ -97,6 +113,8 @@ def generate_dungeon(
             # Dig out a tunnel between this room and the previous one.
             for x, y in tunnel_between(rooms[-1].center, new_room.center):
                 dungeon.tiles[x, y] = tile_types.floor
+
+        place_entities(new_room, dungeon, max_monsters_per_room)
 
         # Append new room to room list
         rooms.append(new_room)
